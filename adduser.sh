d6d3f0928161de87a6cb9e3fa93796ea7fc61e7c
ly88321 <43890984+ly88321@users.noreply.github.com>
2025-03-07 16:39:49 +0700

    adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# 检查参数
-if [ "$#" -ne 3 ]; then
-  echo "用法: $0 <用户名> <密码> <分配IP>"
+# 检查用户名和密码参数
+if [ "$#" -ne 2 ] && [ "$#" -ne 3 ]; then
+  echo "用法: $0 <用户名> <密码> [<分配IP>]"
   exit 1
 fi
 
 USERNAME=$1
 PASSWORD=$2
-IP=$3
+IP=${3:-"*"}  # 如果没有提供 IP，默认使用 "*"
 
 # 添加用户到 chap-secrets
 echo "$USERNAME * $PASSWORD $IP" | sudo tee -a /etc/ppp/chap-secrets > /dev/null
@@ -17,3 +17,4 @@ echo "$USERNAME * $PASSWORD $IP" | sudo tee -a /etc/ppp/chap-secrets > /dev/null
 sudo kill -HUP $(pgrep pppd)
 
 echo "用户 $USERNAME 添加成功！"
+
